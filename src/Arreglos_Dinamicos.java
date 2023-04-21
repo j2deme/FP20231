28a8b061ce8906aa852e0f6274561dad67964f4f
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2023-04-21 12:31:07 -0600

    Update Arreglos_Dinamicos.java
    Agrega un menú para el manejo de la lista de compras.
    
    Incluye documentación adicional.

--- src/Arreglos_Dinamicos.java
+++ src/Arreglos_Dinamicos.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 /**
- * Simulación de lista de compras con arreglos dinámicos (ArrayList)
+ * Simulación de lista de compras con arreglos dinámicos (ArrayList).
+ * Implementa el uso de las funciones:
+ * - size, para el tamaño del arreglo
+ * - add, para añadir un elemento al arreglo
+ * - remove, para eliminar un elemento del arreglo (si existe)
+ * - clear, para vaciar el arreglo
+ * - isEmpty, para determinar si el arreglo está vacío
+ * - get, para obtener el elemento en la posición dada
+ * - contains, para determinar el sí elemento dado, existe en el arreglo
  *
  * @author MSC. Jaime Jesús Delgado Meraz
- * @version 0.1, 2023-04-20
+ * @version 1.0, 2023-04-21
  */
 public class Arreglos_Dinamicos {
   public static final Scanner teclado = new Scanner(System.in);
@@ -38,5 +46,55 @@ public class Arreglos_Dinamicos {
     // Borrar un elemento del arreglo, sin especificar su índice
     lista.remove("🌵");
     System.out.println(lista); // [🥛,🍍,🥚]
+
+    // Vacía la lista, reinicia su tamaño a 0
+    lista.clear();
+
+    int op = 0;
+    String articulo;
+    do {
+      System.out.println("= Lista de Compras 📃=");
+      System.out.println("¿Que quieres hacer?");
+      System.out.print("1. Ver artículos\n2. Agregar artículo\n3. Eliminar artículo\n4. Buscar\n0. Salir\n> ");
+      op = teclado.nextInt();
+      switch (op) {
+        case 1 -> {
+          // isEmpty devuelve true o false, según haya elementos en la lista
+          if (lista.isEmpty()) {
+            System.out.println("No hay artículos en la lista");
+          } else {
+            for (int i = 0; i < lista.size(); i++) {
+              System.out.printf("%d: %s\n", i + 1, lista.get(i));
+            }
+          }
+        }
+        case 2 -> {
+          System.out.print("¿Que quieres agregar?\n> ");
+          articulo = teclado.next();
+          lista.add(articulo);
+          System.out.printf("Se añadió %s a la lista\n", articulo);
+        }
+        case 3 -> {
+          System.out.print("¿Que quieres quitar?\n> ");
+          articulo = teclado.next();
+          // Se utiliza "contains" como condición de paro, ya que mientras
+          // exista el elemento buscado en la lista, el ciclo se repite
+          while (lista.contains(articulo)) {
+            lista.remove(articulo);
+          }
+          System.out.printf("Se quitó %s de la lista\n", articulo);
+        }
+        case 4 -> {
+          System.out.print("¿Que quieres buscar?\n> ");
+          articulo = teclado.next();
+          if (lista.contains(articulo)) {
+            System.out.printf("Hay %s en la lista.\n", articulo);
+          } else {
+            System.out.printf("No hay %s en la lista.\n", articulo);
+          }
+        }
+        default -> op = 0;
+      }
+    } while (op != 0);
   }
 }
